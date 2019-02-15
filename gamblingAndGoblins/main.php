5b66675cc47281cae1b7032fdabf1a1088adb3f4
odax <nr10@albion.edu>
2019-02-15 13:52:47 -0500

    working on gambling mechanic

--- gamblingAndGoblins/main.php
+++ gamblingAndGoblins/main.php
@@ -24,6 +24,9 @@
             $this->money = $money;
             $this->health = $health;
         }
+        function setMoney($dif){
+            $this->money += $dif;
+        }
     }
 
     //Instantiation
@@ -43,24 +46,21 @@
     //Gambler's table
     echo "<h4>Gamblers Table</h4>";
     class Table {
-        public $gameType;
         public $bet;
-        function setGameType($type){
-            $this->gameType = $type;
-        }
         function setBet($amount){
             if ($amount) {
             $this->bet = $amount;
             } else echo "Please bet at least 1 gold";
         }
-        function play(){
-            switch($this->gameType) {
+        function play($gameType){
+            switch($gameType) {
                 case "low":
                     $gen = rand(1,10);
                     if ($gen > 3) {
-                        return $this->bet * 1.25;
+                        return $this->bet;
                     } else {
-                        return -$this.bet;
+                        // return -$this->bet;
+                        return "lose";
                     }
                     break;
                 case "med":
@@ -68,7 +68,7 @@
                     if ($gen > 5) {
                         return $this->bet * 1.95;
                     } else {
-                        return -$this.bet;
+                        return -$this->bet;
                     }
                     break;
                 case "high":
@@ -76,12 +76,43 @@
                     if ($gen > 7) {
                         return $this->bet * 2.35;
                     } else {
-                        return -$this.bet;
+                        return -$this->bet;
                     }
                     break;
             }
         }
     }
+
+    //instantiation
+    $newTable = new Table();
+    //logic
+
+    //display
+    ?>
+    <form action="main.php" method="post">
+        Place Bet On Table: <input type="number" name="currentBet">
+        <input type="submit">
+    </form>
+
+
+    <?php
+    $currentBet = $_POST["currentBet"];
+    if ($currentBet){
+        echo "You've placed $currentBet gold on the table <br>";
+    }
+    $newTable->setBet($currentBet);
+    ?>
+    <form action="main.php" method="post">
+        <br>Choose your risk: <br>
+        Low (70/30): <input type="radio" name="gamble" value="low"> <br>
+        Med (50/50): <input type="radio" name="gamble" value="med"> <br>
+        High (30/70): <input type="radio" name="gamble" value="high">
+        <input type="submit">
+    </form><br>
+    <?php
+    $gameType = $_POST["gamble"];
+    $dif = $newTable->play($gameType);
+    echo "$dif";
     echo "<h4>Merchant's Shop</h4>";
     echo "<h4>Wilderness</h4>";
     //playerAttributes
