37b8d4d8e8cfe9ecf0e1170f3f847d973ef53205
odax <nr10@albion.edu>
2019-02-15 13:00:40 -0500

    added gamblers table class

--- gamblingAndGoblins/main.php
+++ gamblingAndGoblins/main.php
@@ -11,6 +11,11 @@
 <body>
     <?php
     //Header
+    echo "<h1>Gambling & Goblins</h1>";
+
+    //Player
+    echo "<h4>Player Stats</h4>";
+
     class Player {
         public $money;
         public $item;
@@ -20,10 +25,6 @@
             $this->health = $health;
         }
     }
-    echo "<h1>Gambling & Goblins</h1>";
-
-    //Player
-    echo "<h4>Player Stats</h4>";
 
     //Instantiation
     $newPlayer = new Player(100, 10);
@@ -38,7 +39,49 @@
         echo "Health: $newPlayer->health <br>";
         echo "Money: $newPlayer->money <br>";
         echo "Item: $currentItem <br>";
+
+    //Gambler's table
     echo "<h4>Gamblers Table</h4>";
+    class Table {
+        public $gameType;
+        public $bet;
+        function setGameType($type){
+            $this->gameType = $type;
+        }
+        function setBet($amount){
+            if ($amount) {
+            $this->bet = $amount;
+            } else echo "Please bet at least 1 gold";
+        }
+        function play(){
+            switch($this->gameType) {
+                case "low":
+                    $gen = rand(1,10);
+                    if ($gen > 3) {
+                        return $this->bet * 1.25;
+                    } else {
+                        return -$this.bet;
+                    }
+                    break;
+                case "med":
+                    $gen = rand(1,10);
+                    if ($gen > 5) {
+                        return $this->bet * 1.95;
+                    } else {
+                        return -$this.bet;
+                    }
+                    break;
+                case "high":
+                $gen = rand(1,10);
+                    if ($gen > 7) {
+                        return $this->bet * 2.35;
+                    } else {
+                        return -$this.bet;
+                    }
+                    break;
+            }
+        }
+    }
     echo "<h4>Merchant's Shop</h4>";
     echo "<h4>Wilderness</h4>";
     //playerAttributes
